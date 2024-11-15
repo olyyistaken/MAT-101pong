0755633857dd8771848fb7bb84a2acbd9466dbe9
Leslye Jeannin <leslye1.jeannin@epitech.eu>
2024-11-15 11:12:00 +0400

    fix -h, stderr, n=0 and paddle reach condition

--- 101pong.py
+++ 101pong.py
@@ -9,17 +9,32 @@
 import sys
 from math import *
 
-def main():
+def usage():
+    print("USAGE")
+    print("    ./101pong x0 y0 z0 x1 y1 z1 n")
+    print("")
+    print("DESCRIPTION")
+    print("    x0  ball abscissa at time t - 1")
+    print("    y0  ball ordinate at time t - 1")
+    print("    z0  ball altitude at time t - 1")
+    print("    x1  ball abscissa at time t")
+    print("    y1  ball ordinate at time t")
+    print("    z1  ball altitude at time t")
+    print("    n   time shift (greater than or equal to zero, integer)")
 
-    if int(len(sys.argv)) == 1:
-        print("no argument")
-        exit (84)
-    if int(len(sys.argv)) < 8:
-        print("too few argument")
-        exit (84)
-    if int(len(sys.argv)) > 8:
-        print("too many argument")
-        exit (84)
+def main():
+    if len(sys.argv) == 2 and sys.argv[1] == "-h":
+        usage()
+        exit(0)
+    if len(sys.argv) == 1:
+        sys.stderr.write("no argument\n")
+        exit(84)
+    if len(sys.argv) < 8:
+        sys.stderr.write("too few argument\n")
+        exit(84)
+    if len(sys.argv) > 8:
+        sys.stderr.write("too many argument\n")
+        exit(84)
     try:
         x0 = float(sys.argv[1])
         y0 = float(sys.argv[2])
@@ -29,36 +44,35 @@ def main():
         z1 = float(sys.argv[6])
         n = int(sys.argv[7])
     except ValueError:
-        print("all argument must be numbers")
-        exit (84)
-    if n <= 0:
-        print ("n must be a postive int")
-        exit (84)
+        sys.stderr.write("all argument must be numbers\n")
+        exit(84)
+    if n < 0:
+        sys.stderr.write("n must be a positive integer\n")
+        exit(84)
 
-    res = x1 - x0
-    res2 = y1 - y0
-    res3 = z1 - z0
-    n1 = (res * n) + x1
-    n2 = (res2 * n) + y1
-    n3 = (res3 * n) + z1
+    vx = x1 - x0
+    vy = y1 - y0
+    vz = z1 - z0
 
-    absolute = sqrt(res ** 2 + res2 ** 2 + res3 ** 2)
-    if (absolute == 0):
-        print("Velocity vector is zero, can't be calculate incidence angle")
-        exit (84)
+    nx = vx * n + x1
+    ny = vy * n + y1
+    nz = vz * n + z1
 
-    res_angle = asin(abs(res3)/absolute)
-    res_angle *= (180/pi)
+    speed = sqrt(vx**2 + vy**2 + vz**2)
+    if speed == 0:
+        sys.stderr.write("Velocity vector is zero\n")
+        exit(84)
+
     print("The velocity vector of the ball is:")
-    print("({:.2f}, {:.2f}, {:.2f})".format(res, res2, res3))
+    print("({:.2f}, {:.2f}, {:.2f})".format(vx, vy, vz))
     print("At time t + {:.0f}, ball coordinates will be:".format(n))
-    print("({:.2f}, {:.2f}, {:.2f})".format(n1, n2, n3))
+    print("({:.2f}, {:.2f}, {:.2f})".format(nx, ny, nz))
 
-    if (z1 <= 0):
+    if vz == 0 or vz * z1 > 0:
         print("The ball won't reach the paddle.")
     else:
+        angle = asin(abs(vz) / speed) * (180 / pi)
         print("The incidence angle is:")
-        print("{:.2f} degrees".format(res_angle))
-    return 0
+        print("{:.2f} degrees".format(angle))
 
 main()
